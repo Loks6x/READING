8b9ccb3b25aa5dd202201178d66a9265e4e1bdec
Loks6x <gumerovtimur152@gmail.com>
2026-06-18 12:08:05 +0300

    Update useReaderStore.ts

--- store/useReaderStore.ts
+++ store/useReaderStore.ts
@@ -4,9 +4,15 @@ import localforage from 'localforage';
 import { ReaderState } from '../types';
 
 const idbStorage: StateStorage = {
-  getItem: async (name: string): Promise<string | null> => (await localforage.getItem(name)) || null,
-  setItem: async (name: string, value: string): Promise<void> => await localforage.setItem(name, value),
-  removeItem: async (name: string): Promise<void> => await localforage.removeItem(name),
+  getItem: async (name: string): Promise<string | null> => {
+    return (await localforage.getItem(name)) || null;
+  },
+  setItem: async (name: string, value: string): Promise<void> => {
+    await localforage.setItem(name, value);
+  },
+  removeItem: async (name: string): Promise<void> => {
+    await localforage.removeItem(name);
+  },
 };
 
 export const useReaderStore = create<ReaderState>()(
